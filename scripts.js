b86854fc3e5926e919f790b55cba95f6ad1a9bfb
DavidLuan <dluans7@gmail.com>
2023-09-23 20:40:47 -0300

    JS inserido
    Valor final da "gorjeta" com algum bug.

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function calculateTip(event) {
+    event.preventDefault();
+    let Bill = document.getElementById('Bill').value;
+    let ServiceQuality = document.getElementById('ServiceQuality').value;
+    let AllPeople = document.getElementById('AllPeople').value;
+
+    if (Bill == '' | ServiceQuality == 0) {
+       alert("Por favor, preencha os campos.")
+       return;
+    }
+
+    if(AllPeople == "" | AllPeople <= 1) {
+      AllPeople = 1;
+      document.getElementById('each').style.display = "none"  
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (Bill * ServiceQuality) / AllPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
+}
+
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
